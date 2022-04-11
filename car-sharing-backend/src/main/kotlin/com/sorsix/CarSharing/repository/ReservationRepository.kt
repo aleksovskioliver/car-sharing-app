415b603c730dbaf9339e057884bc166e86b96964
aleksovskioliver <aleksovskioliver64@gmail.com>
2022-04-11 12:31:10 +0200

    added reservation search functionality

--- car-sharing-backend/src/main/kotlin/com/sorsix/CarSharing/repository/ReservationRepository.kt
+++ car-sharing-backend/src/main/kotlin/com/sorsix/CarSharing/repository/ReservationRepository.kt
@@ -1,9 +1,13 @@
 package com.sorsix.CarSharing.repository
 
+import com.sorsix.CarSharing.domain.Location
 import com.sorsix.CarSharing.domain.Reservation
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ReservationRepository : JpaRepository<Reservation,Long>{
+    fun findAllByPickupLocation(pickupLocation: Location): List<Reservation>
+    fun findALlByDropoutLocation(dropoutLocation: Location): List<Reservation>
+    fun findAllByPickupLocationAndDropoutLocation(pickupLocation: Location,dropoutLocation: Location): List<Reservation>
 }
